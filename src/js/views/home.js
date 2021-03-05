875c1d2f3fad595e6ebe2bb42a5095bfb494c5b8
heidysnj <heidysnunez15@gmail.com>
2021-03-05 14:13:09 +0000

    first commit

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -2,14 +2,17 @@ import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.scss";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+export const Home = () => {
+	return (
+		<div>
+			<div className="container-fluid bg-dark text-white">
+				<h2 className="d-flex justify-content-center">
+					<p>The Meetup Clone</p>
+				</h2>
+				<p className="d-flex justify-content-center">This is a mini project create by 4GeeksAcademy</p>
+				<p className="d-flex justify-content-center">Using: ReactJs, Bootstrap, Moment and React-router</p>
+				<br />
+			</div>
+		</div>
+	);
+};
